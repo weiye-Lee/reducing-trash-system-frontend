7169f8f5978534f4e378ab3fd466ae444b0f0a40
nancy <957382789@qq.com>
2020-09-22 21:45:28 +0800

    部分登录
    部分登录

--- miniprogram/pages/store/store.js
+++ miniprogram/pages/store/store.js
@@ -7,6 +7,51 @@ Page({
   data: {
 
   },
+  doLogin:function(callback = ()=>{}){
+    let that = this;
+    wx.login({
+      success:function(loginRes){
+        // console.log(loginRes);
+        if(loginRes){
+          //获取用户信息
+          wx.getUserInfo({
+            success:function(infoRes){
+              console.log(infoRes,'>>>');
+              //请求服务端的的登录接口
+              wx.request({
+                url: 'http://localhost:8080/api/login',//待填写
+                data:{
+                  code:loginRes.code,//临时登陆凭证
+                  rawData:infoRes.rawData,//用户非敏感信息
+                  signature:infoRes.signature,//签名
+                  encryptedData:infoRes.encryptedData,//用户敏感信息
+                  iv:infoRes.iv//解密算法的向量
+                },
+                success:function(res){
+                  console.log("登陆成功");
+                  // res = res.data;
+                  // if(res.result==0){
+                  //   that.globalData.userInfo = res.userInfo;
+                  //   wx.setStorageSync('userInfo', JSON.stringify(res.userInfo));
+                  //   wx.setStorageSync('loginFlag',res.skey);
+                  //   console.log("skey="+res.skey);
+                  //   callback();
+                  // }else{
+                  //   that.showInfo("res.errmsg");
+                  // }
+                },
+                fail:function(error){
+                  //调用服务端登录接口失败
+                  console.log(error);
+                }
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面加载
